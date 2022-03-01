41a9ec6a50a1146d86c1cb5aeaee1858c08cde8f
Dhruv <bhagatkardhruv2003@gmail.com>
2022-03-01 13:00:15 +0530

    text spammer errors

--- Text Spammer/main.py
+++ Text Spammer/main.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
-message=imput("Message: ")
-n=input('How Many times: ')
+message=input("Message: ")
+n=int(input('How Many times: '))
 time.sleep(5)
-for i range(0,int(n)):
+for i in range(0,int(n)):
     pyautogui.typewrite(msg+'\n')
