d9513d2f50fe365d03ccfda29c702f7748cbfd4a
Dhruv <bhagatkardhruv2003@gmail.com>
2022-03-25 14:57:02 +0530

    Hangman Game

---
+++ Hangman Game/README.md
@@ -0,0 +1,1 @@
+Made Hangman Game with the help of a youtube tutorial video still learning python.
--- Test/main.py
+++ Test/main.py
@@ -1,9 +1,12 @@
+value=False
 
-matrix= [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-]
-for row in matrix:
-  for item in row:
-    print(item)
+def fn1():
+  if value==True:
+    print("Value is True")
+  else:
+    print("value is False")
+
+fn1()
+fn1()
+fn1()
+fn1()
